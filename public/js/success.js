25675d37de156779cbfa95152cf0b3b3920acbfc
Mayank Choudhary <66920751+Mayank151c@users.noreply.github.com>
2021-10-06 15:55:34 +0530

    changes for deployment

--- public/js/success.js
+++ public/js/success.js
@@ -3,4 +3,4 @@ queryString = queryString.substring(1);
 
 document.getElementById("score-text1").textContent="Test ID = "+queryString;
 document.getElementById("heading").innerHTML=`<p style="font-size:2rem;margin:2rem;font-weight:600;">Test created Successfully</p>`;
-document.getElementById("link").innerHTML=`<a style="font-size:1.7rem;margin-top:2rem;color:blue;" href="https://mcqs-app.herokuapp.com/teststart.html?testid=${queryString}">Direct Link</a>`;
+document.getElementById("link").innerHTML=`<a style="font-size:1.7rem;margin-top:2rem;color:blue;" href="https://mcqs1.herokuapp.com/teststart.html?testid=${queryString}">Direct Link</a>`;

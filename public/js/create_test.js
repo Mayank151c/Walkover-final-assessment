dd385d0d7b08d508198a6f93820760cdad437bda
Mayank Choudhary <66920751+Mayank151c@users.noreply.github.com>
2021-10-06 14:36:55 +0530

    Changes in UI and heading included.

--- public/js/create_test.js
+++ public/js/create_test.js
@@ -1,14 +1,3 @@
-// document.getElementById("submit-button").addEventListener("submit",function(e){
-//     handlesubmitbutton(e);
-//     console.log("fsdsfd",e);
-// })
-
-// function handlesubmitbutton(event){
-//     event.preventDefault();
-//     console.log("addquestionloop");
-//     // window.location.href = "/addquestionloop.html";
-// }
-
 document.getElementById("submit-button").onclick = function (event) {
     let value1 = document.getElementById('number_of_question_pool').value;
     let value2 = document.getElementById('number_of_que_user').value;
@@ -17,8 +6,7 @@ document.getElementById("submit-button").onclick = function (event) {
 
     event.preventDefault();
     console.log("hola");
-    // var value1="foo"; 
-// var value2="bar"; 
+    
 var queryString = "?id=" + value3 + "&u" + value2+"&q"+value1+"&t"+value4;
     location.href = "/addquestionloop.html"+queryString;
 };
